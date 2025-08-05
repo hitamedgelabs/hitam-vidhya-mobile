828848c81e577ed7cf3ab4d410305713bde5e717
Mohamed Rifayath <141167029+mohamedrifa@users.noreply.github.com>
2025-08-05 11:04:34 +0530

    testing

--- src/config/api.js
+++ src/config/api.js
@@ -1,8 +1,8 @@
 const config = {
-  API_URL: 'https://k8jldjzj-8000.inc1.devtunnels.ms/api',
-  enroll_url: 'http://10.0.2.2:5173',
+  API_URL: 'https://hitam-vidhya-admin-backend.onrender.com/api',
+  enroll_url: 'https://dev.hitamvidhya.com',
   instagram: 'https://www.instagram.com/hitam_vidhya/',
-  facebook: 'https://www.facebook.com/people/HitamVidhya/61564371044981/?mibextid=qi2Omg&rdid=hH57drNp6qbIdLw5&share_url=https%253A%252F%252Fwww.facebook.com%252Fshare%252F1AP8hVEcHK%252F%253Fmibextid%253Dqi2Omg',
+  facebook: 'https://www.facebook.com/share/1AP8hVEcHK/?mibextid=qi2Omg',
   twitter: 'https://x.com/HitamVidhya',
   linkedin: 'https://www.linkedin.com/company/hitamvidhya/',
 };
